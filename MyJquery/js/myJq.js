b3097d7e1509e83abcc9195b8e9c791fda7947a3
AzeChow <ablaze_killer@163.com>
2015-02-13 08:47:37 +0800

    JQ
    学习源码

---
+++ MyJquery/js/myJq.js
@@ -0,0 +1,24 @@
+﻿ (function(window,undefined){
+           	   
+    var JQuery = (function(){
+    var JQuery = function(selector,context){
+        return new JQuery.fn.init(selector,context,rootJQuery;
+    	},
+
+    JQuery.fn = JQuery.prototype = {
+               	   constructor:JQuery,
+               	   init:function(selector,context,rootJQuery){}
+    }
+
+   JQuery.fn.init.prototype = JQuery.fn;
+
+   JQuery.extend = JQuery.fn.extend = function(){}
+
+   JQuery.extend({});
+
+   return JQuery;
+
+    })();
+
+           	   window.JQuery = window.$ = JQuery;
+})(window);
