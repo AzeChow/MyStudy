b0e178c09fb317f0891c9f8ba94878a8ca3e082c
AzeChow <ablaze_killer@163.com>
2015-03-11 00:14:09 +0800

    枚举进出口标记

---
+++ ablazeAZE/client/src/com/bestway/common/client/params/ImpExpFlag.java
@@ -0,0 +1,27 @@
+package com.bestway.common.client.params;
+
+public enum ImpExpFlag {
+	// 进口标志
+	IMPORT(0),
+	// 出口标志
+	EXPORT(1),
+	// 特殊报关标志
+	SPECIAL(2);
+
+	private int value;
+
+	private ImpExpFlag(int _value) {
+
+		value = _value;
+
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+}
